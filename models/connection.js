925cfbd4f57fbaf3e67641cbe4e582908617fc04
Aurélien <aurelien.catala@outlook.com>
2024-05-13 20:41:43 +0200

    architecture avec express et yarn

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://aureliencatala:kaFt8Gubr3VJ8cR4@cluster0.7f9ecap.mongodb.net/tickethack";
+const connectionString = "mongodb+srv://aureliencatala:kaFt8Gubr3VJ8cR4@cluster0.7f9ecap.mongodb.net/";
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
