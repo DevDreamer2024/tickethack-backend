2f320bfafc6cd8f87c7077797132fc41e9e814bd
Aurélien <aurelien.catala@outlook.com>
2024-05-14 20:16:59 +0200

    backend route ajoute

--- models/trajet.js
+++ models/trajet.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const trajetSchema = mongoose.Schema({
     departure : String,
     arrival : String,
-    //si on doit rajouter un trajet un jour , en oubliant la date, elle sera automatiquement ajoutée à la date du jour
-    //la date dans la base de donnée est dans un objet , je rajoute { type: Date, default: Date.now }
     date : { type: Date, default: Date.now },
     price : Number,
+    reservationStatus : {
+        type: String,
+        enum: ['réservable', 'cart', 'booked'],
+        default: 'réservable'
+    }
 });
 
 const Trajet = mongoose.model('tickets', trajetSchema);
--- routes/trajets.js
+++ routes/trajets.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const moment = require('moment');
-const fetch = require('node-fetch');
-//on a peut etre pas besoin de node fetch, a voir
+
 const Trajet = require('../models/trajet');
 
 // Définition de la route '/test'
@@ -20,7 +19,8 @@ router.get('/test', (req, res) => {
         });
 });
 
-// Définition de la route '/'
+// Définition de la route '/' permettant d'obtenir les trajets disponibles (10 max) pour une date donnée
+//cette route en get utilise req.query pour recuperer les parametres de la requete
 router.get('/', async (req , res) => {
     // Vérifie si les champs 'departure', 'arrival' et 'date' sont présents dans la requête
     if (!req.query.departure || !req.query.arrival || !req.query.date) {
@@ -49,7 +49,7 @@ router.get('/', async (req , res) => {
         const trajets = await Trajet.find({
             departure: req.query.departure,
             arrival: req.query.arrival,
-            date: { $gt: today.toDate() }
+            date: { $gt: requestDate.toDate() }
         }).limit(10).exec();
         // Vérifie si des trajets ont été trouvés
         if (trajets === null || trajets.length === 0) {
@@ -65,5 +65,79 @@ router.get('/', async (req , res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+    //definition de la route '/bookings' permettant de placer un trajet dans cart
+    //toute les routes en post utilise req.body pour recuperer les parametres de la requete
+
+    router.post('/ajouteraupanier',  (req, res) => {
+        Trajet.findOneAndUpdate({ _id  : req.body.id }, { reservationStatus : 'cart' }, { new : true })
+        .then(data => { 
+            if(data === null){
+                res.json({ result : false, error : 'No trajet found'});
+            } else {
+                res.json({ result : true, trajet : data });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error : 'Internal server error'});
+        });
+    });
+
+    //route permettant d'annuler une reservation
+    router.post('/enleverdupanier', (req, res) => {
+        Trajet.findOneAndUpdate({ _id  : req.body.id , reservationStatus : 'cart' }, { reservationStatus : 'réservable' }, { new : true })
+        .then(data => {
+            if(data === null){
+                res.json({ result : false, error : 'No trajet found'});
+            } else {
+                res.json({ result : true, trajet : data });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error : 'Internal server error'});
+        });
+    });
+
+    router.get('/contenudupanier', (req, res) => {
+        Trajet.find({ reservationStatus : 'cart' })
+        .then(data => {
+            res.json({ result : true, trajets : data });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error : 'Internal server error'});
+        });
+    });
+    
+    //route permettant de valider une reservation 
+    router.post('/validerlacommande', (req, res) => {
+        Trajet.findOneAndUpdate({ _id  : req.body.id, reservationStatus : 'cart' }, { reservationStatus : 'booked' }, { new : true })
+        .then(data => {
+            if(data === null){
+                res.json({ result : false, error : 'No trajet found'});
+            } else {
+                res.json({ result : true, trajet : data });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error : 'Internal server error'});
+        });
+    });
+
+    //route permettant de recuperer les trajets dans le panier
+    //route permettant de recuperer les trajets reserves
+    router.get('/trajetbooked', (req, res) => {
+        Trajet.find({ reservationStatus : 'booked' })
+        .then(data => {
+            res.json({ result : true, trajets : data });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error : 'Internal server error'});
+        });
+    });
 
 module.exports = router;
